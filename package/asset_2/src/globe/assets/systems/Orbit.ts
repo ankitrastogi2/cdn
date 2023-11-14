7d3790658494127f51cf5ec380a49cd23d6309f8
KingSlayr <ankitrastogi31200@gmail.com>
2023-11-14 23:59:20 +0530

    test8

--- package/asset_2/src/globe/assets/systems/Orbit.ts
+++ package/asset_2/src/globe/assets/systems/Orbit.ts
@@ -1,11 +1,6 @@
-import { PerspectiveCamera } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 class Orbit extends OrbitControls {
-  constructor(camera: PerspectiveCamera, canvas?: HTMLCanvasElement) {
-    super(camera, canvas);
-  }
-
   tick = () => this.update();
 }
 
