cb106b67780e1e2503799754ff541a8366c258ef
GitHub Action <action@github.com>
2023-11-16 18:00:15 +0000

    Deploy static files

---
+++ build/9/asset_2/src/globe/assets/systems/Globe.ts
@@ -0,0 +1,11 @@
+import threeGlobe from 'three-globe';
+
+class Globe extends threeGlobe {
+  constructor({ waitForGlobeReady, animateIn }: { waitForGlobeReady?: boolean; animateIn?: boolean }) {
+    super({ waitForGlobeReady, animateIn });
+  }
+
+  tick: (delta: number) => void = () => {};
+}
+
+export { Globe };
